c3e79f9d4b393c76eacc07dadeae7d078a5226fa
Jono <jaugust8@jhu.edu>
2019-12-03 23:50:46 -0500

    scrolldown port

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,8 @@
 document.getElementById("resume_btn").onclick = () => {
   window.open("./assets/images/JonoAugustineResume.pdf", "_blank");
 };
+
+document.getElementById("banner").onclick = () => {
+  document.getElementById("page_content").scrollIntoView();
+};
+
