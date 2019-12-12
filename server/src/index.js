466cdaa44610d2740183fcf848aa6542f2fc3b1b
jonoaugustine <jonoaugustine@jhu.edu>
2019-12-12 01:26:26 -0500

    add scripts

--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mailer = require("nodemailer");
 const email = process.env.EMAIL;
 const pass = process.env.PASS;
-const port = process.env.PORT;
+const port = process.env.PORT ? process.env.PORT : 69420;
 
 const server = express();
 
 server.post("/", (req, res) => {
+  console.log(req.body);
   res.send(
     JSON.stringify({
       message: "success"
