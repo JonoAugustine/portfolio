e490f2280ef2b518e431b5d6c1ee966ebe23616b
jonoaugustine <jonoaugustine@jhu.edu>
2019-12-12 03:04:16 -0500

    test

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ server.use(express.urlencoded({ extended: true }));
 const mailer = require("nodemailer");
 const email = process.env.EMAIL;
 const pass = process.env.PASS;
+console.log(email, pass);
 const transporter = mailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
