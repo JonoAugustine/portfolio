bac92fe786336cda9147ba5fb8087ef6b4e78455
JonoAugustine <jaugust8@jhu.edu>
2020-07-19 06:28:12 +0000

    Update projects.js

--- site/src/js/projects.js
+++ site/src/js/projects.js
@@ -23,7 +23,7 @@ export default [
     "BlueTheRobot",
     "https://jonoaugustine.gitlab.io/bluetherobot/",
     null,
-    "The business website for Twitch Streamer & YouTuber BlueTheRobot.",
+    "The official website for Twitch Streamer & YouTuber BlueTheRobot.",
     require("../images/BTR_patch.png")
   ),
   new Project(
