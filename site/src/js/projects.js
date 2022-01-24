68fb42d4dcf7ac6404b4a0ceb3cb5e77f098e383
JonoAugustine <me@jonoaugustine.com>
2022-01-24 20:29:56 +0000

    Remove old projects

--- site/src/js/projects.js
+++ site/src/js/projects.js
@@ -71,76 +71,11 @@ export default [
     ["ParcelJS", "SCSS", "Gitlab CI/CD"]
   ),
   new Project(
-    "Madame BunBun",
-    "https://edenbunbun.web.app/",
-    null,
-    "The official website for the Twitch streamer and commission artist, Madame BunBun. It uses Firebase and ReactJS to make a simple experience for BunBun and her commissioners and my own PayPal SDK to enable a simple invoicing process.",
-    require("../images/cornbob.png"),
-    false,
-    null,
-    [
-      "ReactJS",
-      "PayPal Invoice API",
-      "Firebase",
-      "SCSS",
-      "Gitlab CI/CD",
-      "Mocha",
-    ]
-  ),
-  new Project(
-    "Snek",
-    "https://jonoaugustine.gitlab.io/web-snake-game/",
-    "https://gitlab.com/JonoAugustine/web-snake-game",
-    "Browser-based Snake Game aimed at giving full controll over game settings to the player.",
-    require("../images/snek.gif"),
-    false,
-    null,
-    ["NodeJS", "ParcelJS", "SASS", "Gitlab CI/CD"]
-  ),
-  new Project(
     "Aquatic Mastery",
     "https://youtube.com/aquaticmaster",
     null,
     "A YouTube channel centered around sharing my experiences as an aquarist and showing others how to achieve success in their own aquariums.",
     require("../images/logo.jpg")
-  ),
-  new Project(
-    "BlueTheRobot",
-    "https://bluetherobot.com",
-    null,
-    "The official website for Twitch Streamer & YouTuber BlueTheRobot.",
-    require("../images/bluetherobot.png"),
-    false,
-    null,
-    ["NodeJS", "ParcelJS", "YouTube API", "TwitchTV API", "Gitlab CI/CD"]
-  ),
-  new Project(
-    "Off The Menu",
-    "https://otm-online.firebaseapp.com",
-    "https://gitlab.com/off-the-menu/site",
-    `A web app aiming to make eating out simpler for allergy sufferers and picky eaters. It provides the ability to search items on restaurant menus to determine if they contain potential allergens which the user defines.`,
-    require("../images/otm_logo.png"),
-    true,
-    null,
-    [
-      "React",
-      "NodeJS",
-      "MongoDB",
-      "Mongoose",
-      "Firebase",
-      "SASS",
-      "TailwindCSS",
-    ]
-  ),
-  new Project(
-    "Ouch",
-    "https://imanthny.github.io/Ouch/",
-    "https://github.com/anthnyd/Ouch",
-    `A web app built like a simulation where you and your friends can exist in an Existence together. In an Existence you can chat to each other and perform fun commands.`,
-    require("../images/ouch.png"),
-    false,
-    null,
-    ["MongoDB", "KTor", "Less", "Websocket"]
   ),
   new Project(
     "Evolution Emulator",
@@ -151,23 +86,13 @@ export default [
     true
   ),
   new Project(
-    "ScoreBorda",
-    "https://jonoaugustine.gitlab.io/ScoreBorda/",
-    "https://gitlab.com/JonoAugustine/ScoreBorda",
-    `A system which utilizes the simplicity of binary choices to score and rank "Features" and "Candidates". By presenting only two options in each comparison, the Borda is able to remove much of the overwhelming aspect of comparing and several choices at once.`,
+    "Snek",
+    "https://jonoaugustine.gitlab.io/web-snake-game/",
+    "https://gitlab.com/JonoAugustine/web-snake-game",
+    "Browser-based Snake Game aimed at giving full controll over game settings to the player.",
+    require("../images/snek.gif"),
+    false,
     null,
-    null,
-    null,
-    ["ReactJS", "SASS", "Gitlab CI/CD"]
-  ),
-  new Project(
-    "Team Magma PRO",
-    "https://teammagmapro.web.app",
-    "https://gitlab.com/JonoAugustine/teammagma",
-    `This project aims to simplify & organize the selling, buying, & trading process of Pokemon Revolution Online by providing users with a clean and intuitive web app to find the items and pokemon they need.`,
-    require("../images/psyduck_purple.png"),
-    null,
-    null,
-    ["ReactJS", "Firebase"]
-  ),
+    ["NodeJS", "ParcelJS", "SASS", "Gitlab CI/CD"]
+  )
 ]
